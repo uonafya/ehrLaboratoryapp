51c797d20f0cb4b23fadfe7e2ceeff49c59b5f04
injiri <simoninjiri@gmail.com>
2020-08-27 10:15:54 +0300

    k updated dependancies to suit > kenyaemr enviroment

--- omod/src/main/java/org/openmrs/module/laboratoryapp/page/controller/MainPageController.java
+++ omod/src/main/java/org/openmrs/module/laboratoryapp/page/controller/MainPageController.java
@@ -1,16 +1,12 @@
 package org.openmrs.module.laboratoryapp.page.controller;
 
-import org.openmrs.Concept;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appui.UiSessionContext;
-import org.openmrs.module.referenceapplication.ReferenceApplicationWebConstants;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.page.PageModel;
 import org.openmrs.ui.framework.page.PageRequest;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Set;
 
 /**
  * Created by Dennys Henry on 2/18/2016.
