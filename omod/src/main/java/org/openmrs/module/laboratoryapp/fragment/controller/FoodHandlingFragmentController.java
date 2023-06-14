37377afcea9a07e0481c8ae46c480729f55dc466
ningosi <nicotwendelee@gmail.com>
2023-06-14 14:15:12 +0300

    Add a way to search for the test concepts for the food handling section

--- omod/src/main/java/org/openmrs/module/laboratoryapp/fragment/controller/FoodHandlingFragmentController.java
+++ omod/src/main/java/org/openmrs/module/laboratoryapp/fragment/controller/FoodHandlingFragmentController.java
@@ -1,6 +1,7 @@
 package org.openmrs.module.laboratoryapp.fragment.controller;
 
 import org.apache.commons.lang3.StringUtils;
+import org.openmrs.Concept;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.hospitalcore.LabService;
 import org.openmrs.module.hospitalcore.model.FoodHandlerSimplifier;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class FoodHandlingFragmentController {
 
@@ -60,5 +62,13 @@ public class FoodHandlingFragmentController {
         }
 
         return SimpleObject.fromCollection(simplifierList, uiUtils, "testName", "conceptReference", "description", "creator", "dateCreated");
+    }
+
+    public List<SimpleObject> getTestsConcepts(@RequestParam(value="query") String conceptTerm, UiUtils ui)
+    {
+        List<Concept> conceptsList = Context.getConceptService().getConceptsByName(conceptTerm, Locale.getDefault(), false);
+        List<Concept> conceptsListBasedOnTestClass = Context.getConceptService().getConceptsByClass(Context.getConceptService().getConceptClassByName("Test"));
+        conceptsList.retainAll(conceptsListBasedOnTestClass);
+        return SimpleObject.fromCollection(conceptsList, ui, "conceptId", "names.name", "uuid", "displayString");
     }
 }
