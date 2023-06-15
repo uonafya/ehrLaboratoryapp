8d5103163679142be48699be30a9d171b82566f9
ningosi <nicotwendelee@gmail.com>
2023-06-15 09:33:11 +0300

    Add access control to the page of food handler management

--- omod/src/main/java/org/openmrs/module/laboratoryapp/page/controller/PatientFoodHandlingPageController.java
+++ omod/src/main/java/org/openmrs/module/laboratoryapp/page/controller/PatientFoodHandlingPageController.java
@@ -1,9 +1,12 @@
 package org.openmrs.module.laboratoryapp.page.controller;
 
 import org.openmrs.Patient;
+import org.openmrs.module.kenyaui.annotation.AppPage;
+import org.openmrs.module.laboratoryapp.LaboratoryConstants;
 import org.openmrs.ui.framework.page.PageModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@AppPage(LaboratoryConstants.APP_LABORATORY_APP)
 public class PatientFoodHandlingPageController {
 
     public void controller(PageModel model, @RequestParam("patientId") Patient currentPatient) {
