0841b1c39e70307de6b57a4c97006ad5c5fbfef9
ningosi <nicotwendelee@gmail.com>
2023-06-15 09:08:08 +0300

    Provide patient context to the food handling page

--- omod/src/main/java/org/openmrs/module/laboratoryapp/page/controller/PatientFoodHandlingPageController.java
+++ omod/src/main/java/org/openmrs/module/laboratoryapp/page/controller/PatientFoodHandlingPageController.java
@@ -1,8 +1,12 @@
 package org.openmrs.module.laboratoryapp.page.controller;
 
+import org.openmrs.Patient;
 import org.openmrs.ui.framework.page.PageModel;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class PatientFoodHandlingPageController {
 
-    public void controller(PageModel model) {}
+    public void controller(PageModel model, @RequestParam("patientId") Patient currentPatient) {
+        model.addAttribute("currentPatient", currentPatient);
+    }
 }
